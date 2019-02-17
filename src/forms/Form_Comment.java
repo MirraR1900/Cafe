53a3f4ce1081ed0a2496c94246fd410b43a7578e
MirraR1990 <46891385+MirraR1990@users.noreply.github.com>
2019-02-18 02:42:48 +0300

    Initial commit

---
+++ src/forms/Form_Comment.java
@@ -0,0 +1,37 @@
+package forms;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Form_Comment
+ */
+@WebServlet("/Form_Comment")
+public class Form_Comment extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Form_Comment() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=utf-8"); 
+	
+		PrintWriter writer = response.getWriter();
+		writer.println("Спасибо, что отставили отзыв о нашем кафе.");
+		writer.close();
+		
+}
+
+}
