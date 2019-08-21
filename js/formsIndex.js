4a00f7da7f0424654c2c3133873d3acff7ac3717
MirraR1990 <46891385+MirraR1990@users.noreply.github.com>
2019-08-21 18:01:22 +0300

    commit

--- js/formsIndex.js
+++ js/formsIndex.js
@@ -14,5 +14,41 @@ inputDate.addEventListener('blur', function() {
 });
 inputTime.addEventListener('focus', function() {
     this.type='time';
-})
+});
+
+var phonNumberOrder = document.getElementById('phoneOrder');
+
+window.addEventListener('DOMContentLoaded', function() {
+function setCursorPosition(pos, elem) {
+    elem.focus();
+    if(elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if(elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+    }
+}
+
+function mask(event) {
+    var phone = "+7(___) ___ ____",
+    i = 0,
+    def = phone.replace(/\D/g, ""),
+    val = this.value.replace(/\D/g, "");
+    if(def.length >= val.length) val = def;
+    this.value = phone.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+
+    if(event.type == "blur") {
+        if(this.value.length == 2) this.value = ""
+    } else setCursorPosition(this.value.length, this)
+};
+
+phonNumberOrder.addEventListener('input', mask, false);
+phonNumberOrder.addEventListener('focus', mask, false);
+phonNumberOrder.addEventListener('blur', mask, false);
+
+});
 
