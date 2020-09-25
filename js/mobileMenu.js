2379304f6dd991fb316faecd5a0a67673049414d
Mirra1900 <>
2020-09-26 00:15:43 +0300

    commit

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,15 +1,18 @@
 let buttonMobileNavigation = document.getElementById("buttonMobileNavigation");
 let navigationSite = document.getElementById("navigationSite");
+let flagСondition = false;
 
 function pressButton() {
     if (flagСondition === false) {
         buttonMobileNavigation.style.color = '';
         buttonMobileNavigation.style.color = 'rgb(0, 0, 255)'; 
         tooggleMenu();
+        flagСondition = true;
     } else {
         buttonMobileNavigation.style.color = '';
         buttonMobileNavigation.style.color = 'rgb(0, 0, 0,)'; 
         tooggleMenu();
+        flagСondition = false
     }
 };
 
